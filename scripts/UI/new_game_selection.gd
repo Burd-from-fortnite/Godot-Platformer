977c3d1499e9bbd1f97e143852608a39b77bf47e
Braxton Wludyga <brax.wludyga@gmail.com>
2025-03-21 08:50:44 -0400

    video 10 halfway point

---
+++ scripts/UI/new_game_selection.gd
@@ -0,0 +1,6 @@
+extends Label
+
+func select():
+	Game.main.main_menu.hide()
+	Game.main.main_menu.disable()
+	Game.main.world.load_level("level_1")
---
+++ scripts/main.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var main_menu = $MainMenu
+@onready var pause_menu = $PauseMenu
+@onready var world = $World
+
+func _ready():
+	Game.main = self
+	main_menu.enable()
