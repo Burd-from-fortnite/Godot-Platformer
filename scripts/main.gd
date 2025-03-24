af1f90b155559b42f3ff20f33c473f551f5933db
Braxton Wludyga <brax.wludyga@gmail.com>
2025-03-24 14:51:52 -0400

    video finale

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,7 +3,11 @@ extends Node2D
 @onready var main_menu = $MainMenu
 @onready var pause_menu = $PauseMenu
 @onready var world = $World
+@onready var transition = $Transition
+@onready var parallax = $ParallaxBackround
 
 func _ready():
 	Game.main = self
+	transition.fade_in()
+	main_menu.show()
 	main_menu.enable()
